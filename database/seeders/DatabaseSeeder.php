15d5a08314f2d60934de2c648201230bd61d957e
leon <20190618@cyadmk.com>
2020-11-26 09:18:45 +0800

    add jwt

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::truncate();
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
